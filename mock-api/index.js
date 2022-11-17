bff8ad8e6289a2ada4949e45c226691e8332b9be
Devin Lizardi <dlizardi@berkeley.edu>
2022-11-16 21:42:21 -0500

    fix!: broke storage?

--- mock-api/index.js
+++ mock-api/index.js
@@ -1,29 +1,44 @@
 const controls = {
   numMonthsToGenerate: 2,
+  date: new Date('August 19, 1975 23:15:30'),
   tempRange: {
     min: 45,
     max: 62,
-    format: "F"
+    format: "F",
   },
   humidRange: {
     min: 25,
     max: 45,
-    format: ""
+    format: "",
   },
-  lightRangeMinMax: {
+  lightRange: {
     min: 807,
     max: 1614,
-    format: "lux"
-  }
+    format: "lux",
+  },
+  moistureRange: {},
+  weightRange: {},
 }
 
-const toBreak = () => {
+const generateData = () => {
   const data = { sensors: [] }
-  // Create 1000 users
-  for (let i = 0; i < 1000; i++) {
-    data.sensors.push({ temp: i, humidity: i, light: i, date: new Date() })
+
+  for (let i = 0; i < 30 * controls.numMonthsToGenerate; i++) {
+    controls.date.setDate(i)
+
+    let tempObject = {
+      temp: i,
+      light: 0,
+      humidity: 0,
+      moisture: 0,
+      weight: 0,
+      date: controls.date,
+    }
+
+    console.log(tempObject)
+    data.sensors.push(tempObject)
   }
   return data
 }
 
-module.exports = toBreak
+module.exports = generateData
